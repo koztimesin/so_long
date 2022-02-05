dd4838d4f9f4aa5d9ea810628650a4b8d184e78a
Ismail <a.isumairu01@gmail.com>
2022-02-05 22:14:03 +0300

    added textures, testing mlx library

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: ksaffron <ksaffron@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 18:00:36 by ksaffron          #+#    #+#             */
-/*   Updated: 2022/02/04 18:19:55 by ksaffron         ###   ########.fr       */
+/*   Updated: 2022/02/04 20:15:27 by ksaffron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,15 @@
 
 # include "./minilibx/mlx.h"
 # include "./libft/libft.h"
+
+typedef struct s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
 //# include "./get_next_line/get_next_line.h"
 
 #endif
