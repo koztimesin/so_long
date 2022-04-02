b04392b5516e93b4434c2b9c9bfac53315dcb731
Karthus Saffron <a.isumairu01@gmail.com>
2022-04-02 18:08:54 +0300

    recoded ft_read_map to take address of 'game' structure

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: ksaffron <ksaffron@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 17:14:43 by ksaffron          #+#    #+#             */
-/*   Updated: 2022/03/31 18:14:08 by ksaffron         ###   ########.fr       */
+/*   Updated: 2022/04/02 18:04:45 by ksaffron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 
 int	main(int argc, char **argv)
 {
+	t_game	game;
+
 	if (argc != 2)
 		ft_error();
 	ft_check_name(argv[1]);
+	ft_read_map(&game, argv[1]);
 }
