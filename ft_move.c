eb9982ad8b1285da07dc558b143b938703deeb92
Karthus Saffron <a.isumairu01@gmail.com>
2022-05-26 18:07:30 +0300

    recoded

--- ft_move.c
+++ ft_move.c
@@ -6,7 +6,7 @@
 /*   By: ksaffron <ksaffron@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 17:30:43 by ksaffron          #+#    #+#             */
-/*   Updated: 2022/05/26 18:02:18 by ksaffron         ###   ########.fr       */
+/*   Updated: 2022/05/26 18:07:13 by ksaffron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,6 @@ static void	ft_print_move(t_game *game)
 	game->moves++;
 	ft_putnbr_fd(game->moves, 1);
 	ft_putchar_fd('\n', 1);
-}
-
-int	ft_move(int key, t_game *game)
-{
-	if (key == ESC)
-		ft_close_game(game);
-	if (key == W)
-		ft_move_player(game, 0, -1);
-	if (key == S)
-		ft_move_player(game, 0, 1);
-	if (key == A)
-		ft_move_player(game, -1, 0);
-	if (key == D)
-		ft_move_player(game, 1, 0);
-	return (0);
 }
 
 static void	ft_move_player(t_game *game, int x, int y)
@@ -57,4 +42,19 @@ static void	ft_move_player(t_game *game, int x, int y)
 	game->px = game->px + x;
 	game->py = game->py + y;
 	ft_draw_img(game, PLAYER, game->px * 64, game->py * 64);
+}
+
+int	ft_move(int key, t_game *game)
+{
+	if (key == ESC)
+		ft_close_game(game);
+	if (key == W)
+		ft_move_player(game, 0, -1);
+	if (key == S)
+		ft_move_player(game, 0, 1);
+	if (key == A)
+		ft_move_player(game, -1, 0);
+	if (key == D)
+		ft_move_player(game, 1, 0);
+	return (0);
 }
