c4e0b0db27cab6563b5429816dd2312692168555
Karthus Saffron <a.isumairu01@gmail.com>
2022-05-26 18:14:08 +0300

    added animation and players current asset

--- ft_draw_map_bonus.c
+++ ft_draw_map_bonus.c
@@ -6,13 +6,13 @@
 /*   By: ksaffron <ksaffron@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 16:59:03 by ksaffron          #+#    #+#             */
-/*   Updated: 2022/05/25 19:35:53 by ksaffron         ###   ########.fr       */
+/*   Updated: 2022/05/26 18:05:22 by ksaffron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long_bonus.h"
 
-void	ft_draw_map_bonus(t_game_b *game, char *asset, int x, int y)
+void	ft_draw_img_bonus(t_game_b *game, char *asset, int x, int y)
 {
 	void	*img;
 	int		img_height;
@@ -22,8 +22,7 @@ void	ft_draw_map_bonus(t_game_b *game, char *asset, int x, int y)
 	mlx_put_image_to_window(game->mlx, game->window, img, x, y);
 }
 
-
-int	ft_draw_img_bonus(t_game_b *game)
+int	ft_draw_map_bonus(t_game_b *game)
 {
 	int	x;
 	int	y;
@@ -34,12 +33,15 @@ int	ft_draw_img_bonus(t_game_b *game)
 		x = 0;
 		while (x < game->length)
 		{
-			ft_draw_map_bonus(game, BOX, 0, 0);
-			ft_draw_map_bonus(game, GROUND, x * 64, y * 64);
+			ft_draw_img_bonus(game, GROUND, x * 64, y * 64);
 			if (game->map[y][x] == '1')
-				ft_draw_map_bonus(game, WALL, x * 64, y * 64);
+				ft_draw_img_bonus(game, WALL, x * 64, y * 64);
+			else if (game->map[y][x] == 'P')
+				ft_draw_img_bonus(game, game->img, x * 64, y * 64);
 			else if (game->map[y][x] == 'C')
-				ft_draw_map_bonus(game, TREAT, x * 64, y * 64);
+				ft_draw_img_bonus(game, TREAT, x * 64, y * 64);
+			else if (game->map[y][x] == 'B')
+				ft_draw_img_bonus(game, ENEMY, x * 64, y * 64);
 			x++;
 		}
 		y++;
@@ -47,14 +49,30 @@ int	ft_draw_img_bonus(t_game_b *game)
 	return (0);
 }
 
-// int	ft_next_frame(t_game_b *game)
-// {
-// 	static int	fps = -150;
-// 	char		*steps;
+int	ft_next_frame(t_game_b *game)
+{
+	int		y;
+	int		x;
+	char	*steps;
 
-// 	steps = ft_itoa(game->moves);
-// 	mlx_string_put(game->mlx, game->window, 20, 20, 0x0066FF33, steps);
-// 	free(steps);
-// 	if (game->map[y][x] == 'B')
-// 		ft_draw_map(game, ENEMY, x * 64, y * 64);
-// }
+	y = -1;
+	while (++y < game->height)
+	{
+		x = -1;
+		while (++x < game->length)
+		{
+			if (game->map[y][x] == 'E')
+			{
+				if (game->status != OPEN)
+					ft_draw_img_bonus(game, STONE, x * 64, y * 64);
+				else
+					ft_draw_img_bonus(game, EXIT, x * 64, y * 64);
+			}
+		}
+	}
+	ft_draw_img_bonus(game, BOX, 0, 0);
+	steps = ft_itoa(game->moves);
+	mlx_string_put(game->mlx, game->window, 17, 20, 0x00000000, steps);
+	free(steps);
+	return (0);
+}
--- so_long_bonus.h
+++ so_long_bonus.h
@@ -6,7 +6,7 @@
 /*   By: ksaffron <ksaffron@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 16:04:39 by ksaffron          #+#    #+#             */
-/*   Updated: 2022/05/25 19:35:29 by ksaffron         ###   ########.fr       */
+/*   Updated: 2022/05/26 18:05:43 by ksaffron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 # define SO_LONG_BONUS_H
 
 # define ENEMY "./assets/enemy.xpm"
-# define ENEMY_W "./assets/enemy_w.xpm"
-# define ENEMY_D "./assets/enemy_d.xpm"
-# define ENEMY_A "./assets/enemy_a.xpm"
 # define PLAYER_W "./assets/player_w.xpm"
 # define PLAYER_D "./assets/player_d.xpm"
 # define PLAYER_A "./assets/player_a.xpm"
@@ -31,6 +28,7 @@ typedef struct s_game_b {
 	char	**map;
 	void	*mlx;
 	void	*window;
+	void	*img;
 	int		height;
 	int		length;
 	int		px;
@@ -44,14 +42,10 @@ typedef struct s_game_b {
 void	ft_game_init_bonus(t_game_b *game);
 int		ft_move_bonus(int key, t_game_b *game);
 void	ft_get_info_bonus(t_game_b *game);
-int		ft_draw_img_bonus(t_game_b *game);
+int		ft_draw_map_bonus(t_game_b *game);
 int		ft_next_frame(t_game_b *game);
 void	ft_read_map_bonus(t_game_b *game, char *file);
-void	ft_move_player_w(t_game_b *game, int x, int y);
-void	ft_move_player_s(t_game_b *game, int x, int y);
-void	ft_move_player_a(t_game_b *game, int x, int y);
-void	ft_move_player_d(t_game_b *game, int x, int y);
 int		ft_close_game_bonus(t_game_b *game);
-void	ft_draw_map_bonus(t_game_b *game, char *asset, int x, int y);
+void	ft_draw_img_bonus(t_game_b *game, char *asset, int x, int y);
 
 #endif
