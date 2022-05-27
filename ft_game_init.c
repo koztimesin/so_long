97093691ad72fdf337fb74349276309cdb57a8d7
Karthus Saffron <a.isumairu01@gmail.com>
2022-05-27 16:30:29 +0300

    check for map resolution and segv

--- ft_game_init.c
+++ ft_game_init.c
@@ -6,19 +6,29 @@
 /*   By: ksaffron <ksaffron@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 15:58:54 by ksaffron          #+#    #+#             */
-/*   Updated: 2022/05/26 17:57:49 by ksaffron         ###   ########.fr       */
+/*   Updated: 2022/05/27 16:21:46 by ksaffron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+#include <stdio.h>
 
 void	ft_game_init(t_game *game)
 {
+	int	x;
+	int	y;
 	ft_correct_map(game->map);
 	ft_get_info(game);
 	ft_wrapped_map(game);
 	game->mlx = mlx_init();
+	if (!game->mlx)
+		ft_game_error(game);
 	game->window = mlx_new_window(game->mlx, game->length * 64, \
 	game->height * 64, "so_long");
+	if (!game->window)
+		ft_game_error(game);
+	mlx_get_screen_size(game->mlx, &x, &y);
+	if ((x / 64) > 40 || (y / 64) > 21)
+		ft_game_error(game);
 	ft_draw_map(game);
 }
--- ft_game_init_bonus.c
+++ ft_game_init_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ksaffron <ksaffron@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 17:55:59 by ksaffron          #+#    #+#             */
-/*   Updated: 2022/05/26 17:53:04 by ksaffron         ###   ########.fr       */
+/*   Updated: 2022/05/27 16:22:31 by ksaffron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,12 +58,23 @@ static void	ft_correct_map_bonus(char **map)
 
 void	ft_game_init_bonus(t_game_b *game)
 {
+	int	x;
+	int	y;
 	ft_correct_map_bonus(game->map);
 	ft_get_info_bonus(game);
 	ft_wrapped_map_bonus(game);
 	game->img = PLAYER;
+	if (!game->img)
+		ft_game_error(game);
 	game->mlx = mlx_init();
+	if (!game->mlx)
+		ft_game_error(game);
 	game->window = mlx_new_window(game->mlx, game->length * 64, \
 	game->height * 64, "so_long_bonus");
+	if (!game->window)
+		ft_game_error(game);
+	mlx_get_screen_size(game->mlx, &x, &y);
+	if ((x / 64) > 40 || (y / 64) > 21)
+		ft_game_error(game);
 	ft_draw_map_bonus(game);
 }
