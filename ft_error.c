6ee38a6cf5b89e7cadcee5e200970040341021d1
Karthus Saffron <a.isumairu01@gmail.com>
2022-05-27 16:41:08 +0300

    added new function

--- ft_error.c
+++ ft_error.c
@@ -6,7 +6,7 @@
 /*   By: ksaffron <ksaffron@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 15:59:07 by ksaffron          #+#    #+#             */
-/*   Updated: 2022/05/25 16:02:21 by ksaffron         ###   ########.fr       */
+/*   Updated: 2022/05/27 16:27:46 by ksaffron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_error(char **map)
 {
 	if (map)
 		ft_free_space(map);
-	ft_putstr_fd("Error\n", 1);
+	ft_putstr_fd("Error\n", 2);
 	exit(1);
 }
 
@@ -25,4 +25,13 @@ int	ft_close_game(t_game *game)
 	mlx_destroy_window(game->mlx, game->window);
 	ft_free_space(game->map);
 	exit(0);
+}
+
+void	ft_game_error(t_game *game)
+{
+	mlx_destroy_window(game->mlx, game->window);
+	if (game->map)
+		ft_free_space(game->map);
+	ft_putstr_fd("Error\n", 2);
+	exit(1);
 }
